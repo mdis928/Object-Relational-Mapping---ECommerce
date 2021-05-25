ad11151c7850e8d3b0768579b64b6b53651ec4a3
Michael DiSanto <mdisanto928@gmail.com>
2021-05-25 09:12:21 -0400

    added code to index.js in models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'ProductTag',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "ProductTag",
+})
 
 module.exports = {
   Product,
