f1b5be73584ac720b7775646a00f8ec5487d6ac4
Michael DiSanto <mdisanto928@gmail.com>
2021-05-27 10:56:51 -0400

    commented out onst { json } = require('sequelize/types'). This line of code is not needed

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { json } = require('sequelize/types');
+// const { json } = require('sequelize/types');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { json } = require('sequelize/types');
+// const { json } = require('sequelize/types');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
